ddbb7677ecbdc9ec195a895983e99711f50829ac
lovevrlsq <lovevrlsq@gmail.com>
2013-11-15 03:04:51 +0800

    添加管理者对象的基类

---
+++ zwg-china.author.model/SystemQuota.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zwg_china.model
+{
+    /// <summary>
+    /// 系统设定的高点号配额方案
+    /// </summary>
+    public class SystemQuota : ModelBase
+    {
+        #region 属性
+
+        /// <summary>
+        /// 返点
+        /// </summary>
+        public double Rebate { get; set; }
+
+        /// <summary>
+        /// 方案明细
+        /// </summary>
+        public virtual List<SystemQuotaDetail> Detail { get; set; }
+
+        #endregion
+
+        #region 构造方法
+
+        /// <summary>
+        /// 实例化一个新的系统设定的高点号配额方案
+        /// </summary>
+        public SystemQuota()
+        {
+        }
+
+        /// <summary>
+        /// 实例化一个新的系统设定的高点号配额方案
+        /// </summary>
+        /// <param name="rebate">返点</param>
+        /// <param name="detail">方案明细</param>
+        public SystemQuota(double rebate, List<SystemQuotaDetail> detail)
+        {
+            this.Rebate = rebate;
+            this.Detail = detail;
+        }
+
+        #endregion
+    }
+}
---
+++ zwg-china.author.model/SystemQuotaDetail.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zwg_china.model
+{
+    /// <summary>
+    /// 系统设定的高点号配额方案的方案明细
+    /// </summary>
+    public class SystemQuotaDetail : ModelBase
+    {
+        #region 属性
+
+        /// <summary>
+        /// 返点
+        /// </summary>
+        public double Rebate { get; set; }
+
+        /// <summary>
+        /// 人数
+        /// </summary>
+        public int Sum { get; set; }
+
+        #endregion
+
+        #region 构造方法
+
+        /// <summary>
+        /// 实例化一个新的系统设定的高点号配额方案的方案明细
+        /// </summary>
+        public SystemQuotaDetail()
+        {
+        }
+
+        /// <summary>
+        /// 实例化一个新的系统设定的高点号配额方案的方案明细
+        /// </summary>
+        /// <param name="rebate">返点</param>
+        /// <param name="sum">人数</param>
+        public SystemQuotaDetail(double rebate, int sum)
+        {
+            this.Rebate = rebate;
+            this.Sum = sum;
+        }
+
+        #endregion
+    }
+}
