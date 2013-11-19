14efad9d250b1d7c011c7ae5582d18200e12959e
lovevrlsq <lovevrlsq@gmail.com>
2013-11-19 21:48:03 +0800

    公告和通知模块的数据模型完成

---
+++ zwg-china.message.model/Bulletin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zwg_china.model
+{
+    /// <summary>
+    /// 公告
+    /// </summary>
+    public class Bulletin : RegularModelBase
+    {
+        #region 公开属性
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 正文
+        /// </summary>
+        public string Context { get; set; }
+
+        #endregion
+
+        #region 构造方法
+
+        /// <summary>
+        /// 实例化一个新的公告
+        /// </summary>
+        public Bulletin()
+        {
+        }
+
+        /// <summary>
+        /// 实例化一个新的公告
+        /// </summary>
+        /// <param name="title">标题</param>
+        /// <param name="context">正文</param>
+        /// <param name="beginTime">开始公示时间</param>
+        /// <param name="endTime">结束时间</param>
+        /// <param name="hide">一个布尔值 标识活动是否暂停（默认为 false）</param>
+        public Bulletin(string title, string context, DateTime beginTime, DateTime endTime, bool hide = false)
+            : base(beginTime, endTime, hide)
+        {
+            this.Title = title;
+            this.Context = context;
+        }
+
+        #endregion
+    }
+}
---
+++ zwg-china.message.model/Notice.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zwg_china.model
+{
+    public class Notice : RecordingTimeModelBase
+    {
+        #region 公开属性
+
+        /// <summary>
+        /// 接收人
+        /// </summary>
+        public virtual Author Owner { get; set; }
+
+        /// <summary>
+        /// 正文
+        /// </summary>
+        public string Context { get; set; }
+
+        /// <summary>
+        /// 一个布尔值 标识该通知是否已经被阅读
+        /// </summary>
+        public bool IsReaded { get; set; }
+
+        /// <summary>
+        /// 来源对象的类型
+        /// </summary>
+        public string SourceType { get; set; }
+
+        /// <summary>
+        /// 目标对象的存储指针
+        /// </summary>
+        public int SourceId { get; set; }
+
+        #endregion
+
+        #region 构造方法
+
+        /// <summary>
+        /// 实例化一个新的通知
+        /// </summary>
+        public Notice()
+        {
+        }
+
+        /// <summary>
+        /// 实例化一个新的通知
+        /// </summary>
+        /// <param name="owner">接收人</param>
+        /// <param name="context">正文</param>
+        /// <param name="sourceType">通知类型</param>
+        /// <param name="sourceId">目标对象的存储指针</param>
+        public Notice(Author owner, string context, string sourceType, int sourceId)
+        {
+            this.Owner = owner;
+            this.Context = context;
+            this.IsReaded = false;
+            this.SourceType = sourceType;
+            this.SourceId = sourceId;
+        }
+
+        #endregion
+    }
+}
