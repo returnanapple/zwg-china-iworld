b33f8338296e3b51c0295d4c988db79b1dd181f5
lovevrlsq <lovevrlsq@gmail.com>
2013-11-20 18:03:37 +0800

    想用户模块追加 返点、佣金和分红 的记录

---
+++ zwg-china.author.model/DividendRecord.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zwg_china.model
+{
+    /// <summary>
+    /// 分红记录
+    /// </summary>
+    public class DividendRecord : RecordingTimeModelBase
+    {
+        #region 属性
+
+        /// <summary>
+        /// 用户
+        /// </summary>
+        public virtual Author Owner { get; set; }
+
+        /// <summary>
+        /// 下线盈亏
+        /// </summary>
+        public double Profit { get; set; }
+
+        /// <summary>
+        /// 分红比例
+        /// </summary>
+        public double Scale { get; set; }
+
+        /// <summary>
+        /// 分红金额
+        /// </summary>
+        public double Sum { get; set; }
+
+        /// <summary>
+        /// 一个布尔值，表示该分红记录是否已经兑现
+        /// </summary>
+        public bool HadHonored { get; set; }
+
+        #endregion
+
+        #region 构造方法
+
+        /// <summary>
+        /// 实例化一个新的分红记录
+        /// </summary>
+        public DividendRecord()
+        {
+
+        }
+
+        /// <summary>
+        /// 实例化一个新的分红记录
+        /// </summary>
+        /// <param name="owner">用户</param>
+        /// <param name="profit">下线盈亏</param>
+        public DividendRecord(Author owner, double profit)
+        {
+            this.Owner = owner;
+            this.Profit = profit;
+            this.Scale = owner.PlayInfo.Dividend;
+            this.Sum = this.Profit > 0 ? 0 : Math.Round(Math.Abs(this.Profit) * this.Scale / 100, 2);
+            this.HadHonored = false;
+        }
+
+        #endregion
+    }
+}
---
+++ zwg-china.author.model/RebateRecord.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zwg_china.model
+{
+    /// <summary>
+    /// 返点记录
+    /// </summary>
+    public class RebateRecord : RecordingTimeModelBase
+    {
+        #region 属性
+
+        /// <summary>
+        /// 用户
+        /// </summary>
+        public virtual Author Owner { get; set; }
+
+        /// <summary>
+        /// 来源
+        /// </summary>
+        public virtual Author Source { get; set; }
+
+        /// <summary>
+        /// 一个布尔值，表示该返点记录是否由直属下级触发
+        /// </summary>
+        public bool IsImmediate { get; set; }
+
+        /// <summary>
+        /// 返点金额
+        /// </summary>
+        public double Sum { get; set; }
+
+        #endregion
+
+        #region 构造方法
+
+        /// <summary>
+        /// 实例化一个新的返点记录
+        /// </summary>
+        public RebateRecord()
+        {
+
+        }
+
+        /// <summary>
+        /// 实例化一个新的返点记录
+        /// </summary>
+        /// <param name="owner">用户</param>
+        /// <param name="source">来源</param>
+        /// <param name="isImmediate">一个布尔值，表示该返点记录是否由直属下级触发</param>
+        /// <param name="sum">返点金额</param>
+        public RebateRecord(Author owner, Author source, bool isImmediate, double sum)
+        {
+            this.Owner = owner;
+            this.Source = source;
+            this.IsImmediate = isImmediate;
+            this.Sum = sum;
+        }
+
+        #endregion
+    }
+}
