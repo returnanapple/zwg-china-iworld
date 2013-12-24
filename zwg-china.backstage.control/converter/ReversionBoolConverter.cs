39b728b14945936eef526f73747bc481c33939b0
lovevrlsq <lovevrlsq@gmail.com>
2013-12-25 00:33:25 +0800

    修改后台选中

---
+++ zwg-china.backstage.control/converter/ReversionBoolConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace zwg_china.backstage.control
+{
+    public class ReversionBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool t = (bool)value;
+            return !t;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool t = (bool)value;
+            return !t;
+        }
+    }
+}
