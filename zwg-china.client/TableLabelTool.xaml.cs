df76e9b67b798c45215fe1b624fcc86899acb9eb
lovevrlsq <lovevrlsq@gmail.com>
2013-12-26 01:33:58 +0800

    TableLabelTool

---
+++ zwg-china.client/TableLabelTool.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace zwg_china.client
+{
+    public partial class TableLabelTool : UserControl
+    {
+        public TableLabelTool()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TableLabelTool), new PropertyMetadata("", (d, e) =>
+            {
+                TableLabelTool td = (TableLabelTool)d;
+                td.Label.Text = (string)e.NewValue;
+            }));
+    }
+}
